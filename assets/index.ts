a5d417252a993ccd09e0d1beb9e633ca2f1e90ac
TJ Draper <tj@buzzingpixel.com>
2023-08-12 16:36:43 -0500

    Add js index file stuff

---
+++ assets/index.ts
@@ -0,0 +1,3 @@
+export { default as PingsBoot } from './PingsBoot';
+export { default as PingsMenuItems } from './PingsMenuItems';
+export { default as PingsRoutes } from './PingsRoutes';
---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.PingsRoutes = exports.PingsMenuItems = exports.PingsBoot = void 0;
+var PingsBoot_1 = require("./PingsBoot");
+Object.defineProperty(exports, "PingsBoot", { enumerable: true, get: function () { return __importDefault(PingsBoot_1).default; } });
+var PingsMenuItems_1 = require("./PingsMenuItems");
+Object.defineProperty(exports, "PingsMenuItems", { enumerable: true, get: function () { return __importDefault(PingsMenuItems_1).default; } });
+var PingsRoutes_1 = require("./PingsRoutes");
+Object.defineProperty(exports, "PingsRoutes", { enumerable: true, get: function () { return __importDefault(PingsRoutes_1).default; } });
